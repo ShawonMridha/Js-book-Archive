d93488e0c6d3d6c3012b921678fe886abde27e12
ShawonMridha <you@example.com>
2021-09-02 00:50:14 +0600

    my sixth assignment

---
+++ js/book.js
@@ -0,0 +1,30 @@
+const searchBook=()=>{
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // console.log(searchText);
+    const url = (`http://openlibrary.org/search.json?q=${searchText}`)
+    fetch(url)
+    .then(response=>response.json())
+    .then(data=>displayResul(data.docs))
+    searchField.value= '';
+}
+
+const displayResul = books=>{
+    console.log(books);
+    const searchResult = document.getElementById('search-result');
+    books.forEach(book=>{
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML=`
+        <div class="card">
+        <div class="card-body">
+        <p class="card-text">${book.title}</p>
+        <h5 class="card-title">Author Name:${book.author_name}</h5>
+        <p class="card-text">${book.publish_year}</p>
+        </div>
+      </div>
+        `;
+        searchResult.appendChild(div)
+    })
+}
+
